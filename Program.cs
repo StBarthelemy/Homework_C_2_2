253b693eac1a7753ec63fa13e801a338b922fc70
StBarthelemy <130822796+StBarthelemy@users.noreply.github.com>
2023-05-01 09:39:07 +0300

    Сохранение результата

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+
+if (number < 100)
+{
+Console.WriteLine("третьей цифры нет");
+}
+else
+{
+
+while (number > 1000)
+{
+    number = number/10;
+}
+Console.WriteLine($"Третья цифра, {number % 10} ");
+}
